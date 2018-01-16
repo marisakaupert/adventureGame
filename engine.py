9ac8c9a96fc1d4b8d7d71054e4adac6db3ce160b
marisaslozano <marisalozano1995@gmail.com>
2018-01-16 15:51:22 -0500

    add engine class to execute the game

---
+++ engine.py
@@ -0,0 +1,15 @@
+from readText import ReadDescriptions
+
+
+class Engine():
+    def __init__(self, rooms_map):
+        pass
+
+    def play(self):
+        pass
+
+    def help(self):
+        pass
+
+    def quit(self):
+        pass
