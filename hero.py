170bab2bc20ce71e1463995fa92135b260463c55
marisaslozano <marisalozano1995@gmail.com>
2018-01-16 15:52:02 -0500

    add hero class to store stuff for the hero

---
+++ hero.py
@@ -0,0 +1,12 @@
+from readText import ReadDescriptions
+
+
+class Hero():
+    def __init__(self, rooms_map):
+        pass
+
+    def inventory(self):
+        pass
+
+    def score(self):
+        pass
