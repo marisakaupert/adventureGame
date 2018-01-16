d05c01f997709ec7b9cbaf0c054393cbd0312d7f
marisaslozano <marisalozano1995@gmail.com>
2018-01-16 16:33:42 -0500

    put tests in a folder

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class RoomInformationTest(unittest.TestCase):
+    """
+    """
+    def test_can_start_in_room_1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
